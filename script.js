9e8f2f1c85866512dec144fea3b627c725b7f701
nicolasbonfim <nicolasdev945@gmail.com>
2021-12-27 15:46:20 -0300

    quinto commit

--- script.js
+++ script.js
@@ -28,7 +28,17 @@ function botao() {
 
 window.addEventListener('scroll', botao);
 
+//aula15
+function digitou(e) {
+   if(e.keyCode == 13 && e.ctrlKey == true) { //Enter
+    let texto = document.getElementById("campo").value;
 
+    console.log(texto);
+   }
+}
+//aula16
+
+    
 
 
 
