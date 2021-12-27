23566ab6157e3e4a08c7f8aa0899a677b9d81b44
nicolasbonfim <nicolasdev945@gmail.com>
2021-12-27 12:48:29 -0300

    quarto commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//aula12
+function trocarImagem(filename, animalname) {
+    document.querySelector('.imagem').setAttribute('src', 'images/' + filename);
+    document.querySelector('.imagem').setAttribute('data-animal', animalname);
+    
+
+}
+function pegarAnimal() {
+    let animal = document.querySelector('.imagem').getAttribute('data-animal');
+    alert("o animal é :" + animal)
+}
+//aula13
+function subirtela() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+    
+}
+//aula13
+function botao() {
+    if(window.scrollY === 0) {
+        document.querySelector('.scrollbutton').style.display = 'none';
+    }else
+     document.querySelector('.scrollbutton').style.display = 'block';
+}
+
+window.addEventListener('scroll', botao);
+
+
+
+
+
