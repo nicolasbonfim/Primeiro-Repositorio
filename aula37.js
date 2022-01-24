6bd2ba59991abc0f4eb28e458cda4e5131eebd08
nicolasbonfim <nicolasdev945@gmail.com>
2022-01-24 17:46:13 -0300

    décimo terceiro commit

---
+++ aula37.js
@@ -0,0 +1,10 @@
+//Código sincrono
+let nome = 'nicolas'
+let sobrenome = 'correa'
+let completo = nome+' '+sobrenome 
+
+//Código assincrono
+let nome = 'nicolas'
+let sobrenome = 'correa'
+let temperatura = Maquininha.pegarTemperatura();
+let completo = nome+' '+sobrenome 
